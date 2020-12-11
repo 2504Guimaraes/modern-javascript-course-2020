890efd86b44a7acdf7ac6d7866ed4b98ecf7a71d
2504Guimaraes <guimaraes.ivan@hotmail.com>
2020-12-11 18:16:16 -0300

    adding 'lesssons' folder
    folder made to the fulture lessons which I'll put inside it

---
+++ lessons/variablesAndConstants.js
@@ -0,0 +1,23 @@
+var a = 3
+let b = 4
+
+var a = 30
+b = 40
+
+console.log(a,b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+const c = 5
+c = 50
+
+/*
+    Showing simple assignment and reassignment in different kinds of
+    variables and how it doesn't works with constants.
+
+    It will just throw an error saying you can not reassign a constant
+    variable.
+*/
