6944d3dbf7a129f522936cb02b896788bea7d67b
2504Guimaraes <guimaraes.ivan@hotmail.com>
2020-12-11 18:30:42 -0300

    adding class's lesson about how js is weakly typed or untyped

---
+++ lessons/untyped.js
@@ -0,0 +1,23 @@
+/*
+    Javascript has no type declarations, and by that fact,
+    it can have some ambiguities that you will naturally need
+    work with :D
+*/
+
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
+
+/*
+
+    Being aware of that, try to avoid using generic names and 
+    abbreviations / acronyms, like:
+ 
+*/
+
+let valor = ''
+let numero = 1
