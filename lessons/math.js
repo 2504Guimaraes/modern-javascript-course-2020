c527b2ac8775a4ceef37a2a6a84bb9e924270348
2504Guimaraes <guimaraes.ivan@hotmail.com>
2020-12-11 21:20:30 -0300

    adding file with a sample about the math class

---
+++ lessons/math.js
@@ -0,0 +1,10 @@
+/* 
+    The math Class is really useful
+    to work with numbers too 
+*/
+
+const raio = 5.6
+const area = Math.PI * Math.pow(raio, 2)
+
+console.log(area)
+console.log(typeof Math)
