0b9c8b65192ca27dfb45637daa6453b74ff4e96b
2504Guimaraes <guimaraes.ivan@hotmail.com>
2020-12-16 21:39:38 -0300

    updating "thisAndBind.js" based on "thisCanVary.js"
    updating file based in knowledge acquired with thisCanViry.js

--- lessons/functions/thisAndBind.js
+++ lessons/functions/thisAndBind.js
@@ -9,6 +9,10 @@ pessoa.falar()
 const falar = pessoa.falar
 falar() // conflict between paradigms, functional and OO.
 
+// Object Oriented would try to call "pessoa" and functional is calling 
+// "window" or "document" object (it'll depend if you're in brownser
+// or inside node.js).
+
 const falarDePessoa = pessoa.falar.bind(pessoa)
 
 falarDePessoa()
