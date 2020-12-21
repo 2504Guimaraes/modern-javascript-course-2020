45fc6d97ca31c2aeb69a62dd34481886b18582b9
2504Guimaraes <guimaraes.ivan@hotmail.com>
2020-12-21 17:16:37 -0300

    adding exercises from exercise list

---
+++ lessons/functions/Exercise-List(Fundamentals, Control Structures and Functions)/exe01.js
@@ -0,0 +1,10 @@
+const mathOperations = (a, b) => {
+    console.log({
+        soma: a * b,
+        subtracao: a - b,
+        multiplicacao: a * b,
+        divisao: a / b
+    })
+}
+
+mathOperations(5,5)
