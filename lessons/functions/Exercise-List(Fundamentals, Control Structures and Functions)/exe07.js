298438c4dbcd2bbb5dd502da5cc9d4e314479f70
2504Guimaraes <guimaraes.ivan@hotmail.com>
2020-12-22 03:23:02 -0300

    seventh exercise done

---
+++ lessons/functions/Exercise-List(Fundamentals, Control Structures and Functions)/exe07.js
@@ -0,0 +1,12 @@
+const funcBhaskara = (a, b, c) => {
+
+    const delta = (b * b) - (4 * a * c)
+    const x1 = ((-b) + Math.sqrt(delta)) / (2 * a)
+    const x2 = ((-b) - Math.sqrt(delta)) / (2 * a)
+
+    return delta >= 0 ? [x1, x2] : 'Delta é negativo.'
+}
+
+console.log(funcBhaskara(3, -5, 12))
+console.log(funcBhaskara(1, 12, -13))
+console.log(funcBhaskara(2, -16, -18))
