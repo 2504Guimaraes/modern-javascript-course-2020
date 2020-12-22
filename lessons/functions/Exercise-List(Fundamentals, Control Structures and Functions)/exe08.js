fdd48c2d4f0d45dfb526b35ab36d321295cded30
2504Guimaraes <guimaraes.ivan@hotmail.com>
2020-12-22 17:33:18 -0300

    it looks like I misunderstood the real exercise's goal :P
    exercice 8, corrected version

--- lessons/functions/Exercise-List(Fundamentals, Control Structures and Functions)/exe08.js
+++ lessons/functions/Exercise-List(Fundamentals, Control Structures and Functions)/exe08.js
@@ -1,36 +1,34 @@
-const pontuacoes = [6,7,5,8,9,9,4,3,10,7]
+const verDesempenho = listaString => {
 
-const verDesempenho = lista_de_Pontuacoes => {
+  const lista_de_Pontuacoes = listaString.split(", ")
 
-    let i = 0
-    let maiorDesempenho = lista_de_Pontuacoes[0]
-    let menorDesempenho = lista_de_Pontuacoes[0]
-    let qnt_Recordes = []
+  let i = 0
+  let maiorDesempenho = lista_de_Pontuacoes[0]
+  let menorDesempenho = lista_de_Pontuacoes[0]
+  let qnt_Recordes = []
+  let localMenorDesempenho = 'Não há melhor desempenho'
 
-    while(i < lista_de_Pontuacoes.length) {
 
-        if ( maiorDesempenho < lista_de_Pontuacoes[i]) {
-          maiorDesempenho = lista_de_Pontuacoes[i]
-          qnt_Recordes.push(
-            lista_de_Pontuacoes[i]
-          )
-        }
+  while(i < lista_de_Pontuacoes.length) {
 
-        if ( menorDesempenho > lista_de_Pontuacoes[i]) {
-          menorDesempenho = lista_de_Pontuacoes[i]
-        }
+      if ( maiorDesempenho < lista_de_Pontuacoes[i]) {
+        maiorDesempenho = lista_de_Pontuacoes[i]
+        qnt_Recordes.push(
+          lista_de_Pontuacoes[i]
+        )
+      }
 
-        i++
-    }
+      if ( menorDesempenho > lista_de_Pontuacoes[i]) {
+        menorDesempenho = lista_de_Pontuacoes[i]
+        localMenorDesempenho = i + 1
+      }
 
-    return [
-        {'lista de Records': qnt_Recordes},
-        {'Nº vezes que bateu o próprio record': qnt_Recordes.length},
-        {'pior nota já tirada': menorDesempenho}
-    ]
+      i++
+  }
+
+  return [qnt_Recordes.length, localMenorDesempenho]
 }
 
-console.log(verDesempenho(pontuacoes))
-console.log(verDesempenho([9,8,7,5]))
-console.log(verDesempenho([5,6,5,5]))
-console.log(verDesempenho([5,5,5,5]))
+console.log(verDesempenho('9 , 8, 7, 5'))
+console.log(verDesempenho('5 , 6, 5, 5'))
+console.log(verDesempenho('5, 5, 5, 5'))
