7940e86ac38e7a0226960ee07d5807433870a919
2504Guimaraes <guimaraes.ivan@hotmail.com>
2020-12-25 03:36:48 -0300

    adding tenth exercise

---
+++ lessons/functions/Exercise-List(Fundamentals, Control Structures and Functions)/exe10.js
@@ -0,0 +1,19 @@
+const ver = func => console.log(func)
+
+const divisivelPor3 = numero => {
+    return numero % 3 == 0 ? true : false
+}
+
+ver(divisivelPor3(6))
+ver(divisivelPor3(9))
+ver(divisivelPor3(12))
+ver(divisivelPor3(15))
+ver(divisivelPor3(4))
+ver(divisivelPor3(-1))
+ver(divisivelPor3(17))
+ver(divisivelPor3(2))
+
+
+
+
+
