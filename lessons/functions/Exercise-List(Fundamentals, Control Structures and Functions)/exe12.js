9f7f4c02e9dc21d8266ebd27a4c1ff5c86edd93d
2504Guimaraes <guimaraes.ivan@hotmail.com>
2020-12-26 00:29:49 -0300

    twelfth exercise accomplished

---
+++ lessons/functions/Exercise-List(Fundamentals, Control Structures and Functions)/exe12.js
@@ -0,0 +1,33 @@
+const fatorialDe = numero => {
+    let fatorial = 1
+    let i = 1
+
+    while(i <= numero) {            
+        if (i < Number.MAX_SAFE_INTEGER) {
+            fatorial *= i
+            i++
+        } 
+        else { return Infinity }
+    }
+    return fatorial
+}
+
+console.time()
+console.log(fatorialDe(1))
+console.log(fatorialDe(2))
+console.log(fatorialDe(3))
+console.log(fatorialDe(4))
+console.log(fatorialDe(5))
+console.log(fatorialDe(8))
+console.log(fatorialDe(10))
+console.log(fatorialDe(12))
+console.log(fatorialDe(14))
+console.log(fatorialDe(16))
+console.log(fatorialDe(21))
+console.log(fatorialDe(26))
+console.log(fatorialDe(31))
+console.log(fatorialDe(36))
+console.log(fatorialDe(51))
+console.log(fatorialDe(151))
+console.log(fatorialDe(251))
+console.timeEnd()
