6f8924a5dced484293627fb0c3617818986b6eb6
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-02-17 22:20:52 -0300

    Folder about Objects in JS has been created

---
+++ lessons/Objects/objectReview.js
@@ -0,0 +1,7 @@
+// an object is dynamic collection of attributes / values
+const produto = new Object
+produto.nome = 'Cadeira'
+produto['marca'] = 'Genérica'
+produto.preco = 200
+
+console.log(produto)
