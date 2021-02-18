1b62e7f796c6fc3f812f7b989ae17c4e1afa83f6
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-02-17 22:38:10 -0300

    showing some objects's features

--- lessons/Objects/objectReview.js
+++ lessons/Objects/objectReview.js
@@ -5,3 +5,38 @@ produto['marca'] = 'Genérica'
 produto.preco = 200
 
 console.log(produto)
+delete produto.preco
+delete produto['marca']
+console.log(produto)
+
+const carro = {
+    modelo: 'A4',
+    valor: 89000,
+    proprietario: {
+        nome: 'Raul',
+        idade: 36,
+        endereco: {
+            logradouro: 'Rua ABC',
+            numero: 123
+        }
+    },
+    condutores: [
+        { nome: 'Junior', idade: 19 },
+        { nome: 'Ana', idade: 42 }
+    ],
+    calcularValorSeguro: function() {
+        // ...
+    }
+}
+
+carro.proprietario.endereco.numero = 1000
+carro['proprietario']['endereco']['logradouro'] = 'Av Gigante'
+
+console.log(carro)
+
+delete carro.condutores
+delete carro.proprietario.endereco
+delete carro.calcularValorSeguro
+
+console.log(carro)
+console.log(carro.condutores)
