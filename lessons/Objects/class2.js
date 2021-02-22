d2242166b0dc62c8223609d9fb9accef6a3540a2
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-02-22 01:53:43 -0300

    putting notes telling that inheritance used in classes and prototype are in truth the same thing

--- lessons/Objects/class2.js
+++ lessons/Objects/class2.js
@@ -19,3 +19,10 @@ class Filho extends Pai {
 
 const filho = new Filho
 console.log(filho)
+
+// Showing that it's actually the same thing as JS functions:
+// And it goes the some for JS inheritance:
+
+console.log(filho.__proto__ === Filho.prototype)
+console.log(filho.__proto__.__proto__ === Pai.prototype)
+console.log(Pai.prototype.__proto__ === Avo.prototype)
