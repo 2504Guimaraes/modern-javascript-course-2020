ded4af47d6936003f911540cb34a7a596f24db0d
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-02-24 20:41:57 -0300

    first file about map method

---
+++ lessons/Arrays/map1.js
@@ -0,0 +1,29 @@
+const nums = [1, 2, 3, 4, 5]
+
+
+// Map is a for but with purpose :P
+
+let result = nums.map((element) => {
+    return element * 2
+})
+
+console.log({ original: nums, new_one: result })
+
+let test = nums.forEach((element) => {
+    return element * 2
+})
+
+console.log(test) // It will returns undefined, 'cause unlike Map, forEach
+// just manipulates one element or value directly inside an array, without
+// generating a new array with your specified features.
+
+
+// You would need to do something like that for the same purpose:
+
+const newArray = []
+
+nums.forEach((value) => {
+   newArray.push(value * 2)
+})
+
+console.log({ new_one: newArray })
