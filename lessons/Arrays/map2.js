926395604d0bac3396a516ca182114ac68746a29
2504Guimaraes <guimaraes.ivan@hotmail.com>
2021-02-25 02:25:03 -0300

    second fileabout map accomplished

---
+++ lessons/Arrays/map2.js
@@ -0,0 +1,15 @@
+const carrinho = [
+    '{ "nome": "Borracha" , "preco": 3.45 }',
+    '{ "nome": "Caderno" , "preco": 13.90 }',
+    '{ "nome": "Kit de Lápis", "preco": 41.99 }',
+    '{ "nome": "Caneta", "preco": 7.50 }'    
+]
+
+// Return an array with just the JSON objects's price.
+
+const prices = carrinho.map(elemnt => {
+    const el = JSON.parse(elemnt)
+    return el.preco
+})
+
+console.log(prices)
